c8207f55ae261e54d1785896b88ff957ba1b30ab
Franccesco Felipe Rodrigues <ffresanto@gmail.com>
2020-05-22 21:12:14 -0300

    menu com javascript

--- js/main.js
+++ js/main.js
@@ -3,7 +3,10 @@
     $body.classList.remove('no-js')
     $body.classList.add('js')
 
-    var $btnMenu = document.querySelector('.header__btnMenu');
-    $btnMenu.removeAttribute('style')
+    var menu = new Menu({
+        container: '.header__nav',
+        toggleBtn: '.header__btnMenu',
+        widthEnabled: 1024
+    })
 })()
 
